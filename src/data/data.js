ce1675385ce65991a63f4ba5e8acdb55851110f9
afreen0102 <afreenw08@gmail.com>
2023-07-20 18:06:39 +0530

    added the fast-react-pizza

--- src/data/data.js
+++ src/data/data.js
@@ -1,8 +1,17 @@
 // import WorkIm from '../assets/projects/workImg.jpeg'
 // import RealEst from '../assets/projects/realestate.jpg'
-import worldwise from '../assets/worldwise.png'
+import worldwise from '../assets/worldwise.png';
+import pizza from '../assets/fast-react-pizza.png';
 
 export const data=[
+    {
+        id:2,
+        name:"Fast-React-Pizza",
+        image: pizza,
+        github:"https://github.com/afreen0102/Fast-React-Pizza",
+        live:"https://super-kitsune-6d4a37.netlify.app/",
+        description: "A fast and responsive pizza ordering web application."
+    },
     {
         id:1,
         name:"WorldWise",
